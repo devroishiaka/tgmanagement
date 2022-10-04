9ce3bd9db3405e2bfa6a53716016fa25980c77b5
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 20:44:16 +0530

    Update about.py

--- Lumine/modules/about.py
+++ Lumine/modules/about.py
@@ -1,11 +1,15 @@
 __help__ = """
-** ✨ Ruka Sarashina - A Powerful Advance Telegram Group Manager Bot ✨ **
-Powerful Abilities
-•  ❤️
-• File To Link and URL Upload 📂
-• Youtube Downloader 🎵
+** ✨ Eri Akane - A Powerful Advance Telegram Fun Fantasy Bot ✨ **
 
-Developer [Ishikki Akabane](https://t.me/ishikki_akabane)
-Support Group [Support](https://t.me/suppportXD)
+Hello everyone,
+I’m Eri Akane. Nice to meet you.
+I’m a fun bot specially made for [Friends Domain Group](t.me/FriendsDomain).
+
+My developer and owner is [Ishikki Akabane](t.me/ishikki_akabane).
+Currently I’m still in beta phase which means you can’t add me in your groups. If you still wish to add me in your groups you must talk with @ishikki_akabane.
+You can see me fully working in @friendsDomain group, please visit the group and play with me.
+if you want to know about the game or how to play, then please send /howto
+
+If you have any doubts then simply message my babe [Ishikki Akabane](t.me/ishikki_akabane)
 """
 __mod_name__ = "About"
