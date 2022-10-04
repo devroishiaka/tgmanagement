de60491224760bc718e4ed2f340f97cecdd6ea27
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 21:54:03 +0530

    Update purge.py

--- Lumine/modules/purge.py
+++ Lumine/modules/purge.py
@@ -86,11 +86,5 @@ async def delete_messages(event):
     await event.client.delete_messages(chat, del_message)
 
 
-__help__ = """
-*Admin only:*
- • `/del`: deletes the message you replied to
- • `/purge`: deletes all messages between this and the replied to message.
- • `/purge <integer X>`: deletes the replied message, and X messages following it if replied to a message.
-"""
 
 __mod_name__ = "Purges"
