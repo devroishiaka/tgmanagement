60d08f3c86230eac0a89b342d6a949c6a30617b9
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 21:24:13 +0530

    Update animequotes.py

--- Lumine/modules/animequotes.py
+++ Lumine/modules/animequotes.py
@@ -1,8 +1,7 @@
-# Made By @Madepranav On Telegram & Github Id Superboyfan
 import random
 
 from telegram import Update
-from telegram.ext import CallbackContext, run_async
+from telegram.ext import CallbackContext, CommandHandler, run_async
 
 import Lumine.modules.animequotesstring as animequotesstring
 from Lumine import dispatcher
@@ -13,6 +12,13 @@ def animequotes(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(animequotesstring.ANIMEQUOTES))
 
 
-ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("animequotes", animequotes, run_async=True)
+def animequotespic(update: Update, context: CallbackContext):
+    update.effective_message.reply_photo(random.choice(animequotesstring.QUOTES_IMG))
+    
+
+ANIMEQUOTES_HANDLER = DisableAbleCommandHandler("quotes", animequotes, run_async=True)
+ANIMEQUOTESPIC_HANDLER = DisableAbleCommandHandler("animequotes", animequotespic, run_async=True)
+
 
 dispatcher.add_handler(ANIMEQUOTES_HANDLER)
+dispatcher.add_handler(ANIMEQUOTESPIC_HANDLER)
