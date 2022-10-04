44b172d5606e579d2bddb3da84b2c2e419f5f9cc
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 21:35:30 +0530

    Update disable.py

--- Lumine/modules/disable.py
+++ Lumine/modules/disable.py
@@ -317,12 +317,12 @@ if is_module_loaded(FILENAME):
     def __chat_settings__(chat_id, user_id):
         return build_curr_disabled(chat_id)
 
-    DISABLE_HANDLER = CommandHandler("disable", disable, run_async=True)
-    DISABLE_MODULE_HANDLER = CommandHandler("disablemodule", disable_module, run_async=True)
-    ENABLE_HANDLER = CommandHandler("enable", enable, run_async=True)
-    ENABLE_MODULE_HANDLER = CommandHandler("enablemodule", enable_module, run_async=True)
-    COMMANDS_HANDLER = CommandHandler(["cmds", "disabled"], commands, run_async=True)
-    TOGGLE_HANDLER = CommandHandler("listcmds", list_cmds, run_async=True)
+    DISABLE_HANDLER = CommandHandler("kdisable", disable, run_async=True)
+    DISABLE_MODULE_HANDLER = CommandHandler("kdisablemodule", disable_module, run_async=True)
+    ENABLE_HANDLER = CommandHandler("kenable", enable, run_async=True)
+    ENABLE_MODULE_HANDLER = CommandHandler("kenablemodule", enable_module, run_async=True)
+    COMMANDS_HANDLER = CommandHandler(["kcmds", "kdisabled"], commands, run_async=True)
+    TOGGLE_HANDLER = CommandHandler("klistcmds", list_cmds, run_async=True)
 
     dispatcher.add_handler(DISABLE_HANDLER)
     dispatcher.add_handler(DISABLE_MODULE_HANDLER)
@@ -331,16 +331,6 @@ if is_module_loaded(FILENAME):
     dispatcher.add_handler(COMMANDS_HANDLER)
     dispatcher.add_handler(TOGGLE_HANDLER)
 
-    __help__ = """
-    • `/cmds`*:* check the current status of disabled commands
-
-    *Admins only:*
-    • `/enable <cmd name>`*:* enable that command
-    • `/disable <cmd name>`*:* disable that command
-    • `/enablemodule <module name>`*:* enable all commands in that module
-    • `/disablemodule <module name>`*:* disable all commands in that module
-    • `/listcmds`*:* list all possible toggleable commands
-    """
 
     __mod_name__ = "Disable Module"
 
