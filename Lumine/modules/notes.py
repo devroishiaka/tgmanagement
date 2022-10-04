0896b9a7616adab39647e6cec16f804541a53f5e
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 21:52:43 +0530

    Update notes.py

--- Lumine/modules/notes.py
+++ Lumine/modules/notes.py
@@ -573,45 +573,17 @@ def __chat_settings__(chat_id, user_id):
     notes = sql.get_all_chat_notes(chat_id)
     return f"There are `{len(notes)}` notes in this chat."
 
-
-__help__ = """
- • `/get <notename>`*:* get the note with this notename
- • `#<notename>`*:* same as /get
- • `/notes` or `/saved`*:* list all saved notes in this chat
- • `/number` *:* Will pull the note of that number in the list
-If you would like to retrieve the contents of a note without any formatting, use `/get <notename> noformat`. This can \
-be useful when updating a current note
-
-*Admins only:*
- • `/save <notename> <notedata>`*:* saves notedata as a note with name notename
-A button can be added to a note by using standard markdown link syntax - the link should just be prepended with a \
-`buttonurl:` section, as such: `[somelink](buttonurl:example.com)`. Check `/markdownhelp` for more info
- • `/save <notename>`*:* save the replied message as a note with name notename
- Separate diff replies by `%%%` to get random notes
- *Example:* 
- `/save notename
- Reply 1
- %%%
- Reply 2
- %%%
- Reply 3`
- • `/clear <notename>`*:* clear note with this name
- • `/removeallnotes`*:* removes all notes from the group
- *Note:* Note names are case-insensitive, and they are automatically converted to lowercase before getting saved.
- • `/privatenotes <on/yes/1/off/no/0>`: enable or disable private notes in chat
-"""
-
 __mod_name__ = "Notes"
 
-GET_HANDLER = CommandHandler("get", cmd_get, run_async=True)
+GET_HANDLER = CommandHandler("kget", cmd_get, run_async=True)
 HASH_GET_HANDLER = MessageHandler(Filters.regex(r"^#[^\s]+"), hash_get, run_async=True)
 SLASH_GET_HANDLER = MessageHandler(Filters.regex(r"^/\d+$"), slash_get, run_async=True)
-SAVE_HANDLER = CommandHandler("save", save, run_async=True)
-DELETE_HANDLER = CommandHandler("clear", clear, run_async=True)
+SAVE_HANDLER = CommandHandler("ksave", save, run_async=True)
+DELETE_HANDLER = CommandHandler("kclear", clear, run_async=True)
 
-LIST_HANDLER = DisableAbleCommandHandler(["notes", "saved"], list_notes, admin_ok=True, run_async=True)
+LIST_HANDLER = DisableAbleCommandHandler(["knotes", "ksaved"], list_notes, admin_ok=True, run_async=True)
 
-CLEARALL = DisableAbleCommandHandler("removeallnotes", clearall, run_async=True)
+CLEARALL = DisableAbleCommandHandler("kremoveallnotes", clearall, run_async=True)
 CLEARALL_BTN = CallbackQueryHandler(clearall_btn, pattern=r"notes_.*", run_async=True)
 
 dispatcher.add_handler(GET_HANDLER)
