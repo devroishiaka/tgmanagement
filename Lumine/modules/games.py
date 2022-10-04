cd8d8d5f16eafd95ed635d2f732f056c5ded35aa
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 21:38:43 +0530

    Update games.py

--- Lumine/modules/games.py
+++ Lumine/modules/games.py
@@ -27,33 +27,21 @@ def dare(update: Update, context: CallbackContext):
 def tord(update: Update, context: CallbackContext):
     update.effective_message.reply_text(random.choice(game_strings.TORD_STRINGS))
 
-@run_async
-def wyr(update: Update, context: CallbackContext):
-    update.effective_message.reply_text(random.choice(game_strings.WYR_STRINGS))
 
-
-__help__ = """
- • `/truth`*:* asks you a question
- • `/dare`*:* gives you a dare
- • `/tord`*:* can be a truth or a dare
- • `/rather`*:* would you rather
-  """
 
 TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth)
 DARE_HANDLER = DisableAbleCommandHandler("dare", dare)
 TORD_HANDLER = DisableAbleCommandHandler("tord", tord)
-WYR_HANDLER = DisableAbleCommandHandler("rather", wyr)
 
 dispatcher.add_handler(TRUTH_HANDLER)
 dispatcher.add_handler(DARE_HANDLER)
 dispatcher.add_handler(TORD_HANDLER)
-dispatcher.add_handler(WYR_HANDLER)
 
 __mod_name__ = "Games"
 __command_list__ = [
-   "truth", "dare", "tord", "rather",
+   "truth", "dare", "tord",
 ]
 
 __handlers__ = [
-    TRUTH_HANDLER, DARE_HANDLER, TORD_HANDLER, WYR_HANDLER,
+    TRUTH_HANDLER, DARE_HANDLER, TORD_HANDLER,
 ]
