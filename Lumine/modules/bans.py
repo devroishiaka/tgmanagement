92db6a2b6bea6410a84bd39edf20a24a9e3a5b53
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 12:57:32 +0530

    Update bans.py

--- Lumine/modules/bans.py
+++ Lumine/modules/bans.py
@@ -385,24 +385,11 @@ def selfunban(context: CallbackContext, update: Update) -> str:
     return log
 
 
-__help__ = """
- • `/punchme`*:* punchs the user who issued the command
- • `/kickme`*:* same as punchme
-
-*Admins only:*
- • `/ban <userhandle>`*:* bans a user. (via handle, or reply)
- • `/sban <userhandle>`*:* Silently ban a user. Deletes command, Replied message and doesn't reply. (via handle, or reply)
- • `/tban <userhandle> x(m/h/d)`*:* bans a user for `x` time. (via handle, or reply). `m` = `minutes`, `h` = `hours`, `d` = `days`.
- • `/unban <userhandle>`*:* unbans a user. (via handle, or reply)
- • `/punch <userhandle> <reason>(optional)`*:* Punches a user out of the group, (via handle, or reply)
- • `/kick <userhandle>`*:* same as punch
-"""
-
-BAN_HANDLER = DisableAbleCommandHandler(["ban", "sban"], ban, run_async=True)
-TEMPBAN_HANDLER = DisableAbleCommandHandler("tban", temp_ban, run_async=True)
-PUNCH_HANDLER = DisableAbleCommandHandler(["punch", "kick"], punch, run_async=True)
-UNBAN_HANDLER = DisableAbleCommandHandler("unban", unban, run_async=True)
-ROAR_HANDLER = DisableAbleCommandHandler("roar", selfunban, run_async=True)
+BAN_HANDLER = DisableAbleCommandHandler(["kban", "ksban"], ban, run_async=True)
+TEMPBAN_HANDLER = DisableAbleCommandHandler("ktban", temp_ban, run_async=True)
+PUNCH_HANDLER = DisableAbleCommandHandler(["kpunch", "kkick"], punch, run_async=True)
+UNBAN_HANDLER = DisableAbleCommandHandler("kunban", unban, run_async=True)
+ROAR_HANDLER = DisableAbleCommandHandler(["roar", "runban"], selfunban, run_async=True)
 PUNCHME_HANDLER = DisableAbleCommandHandler(["punchme", "kickme"], punchme, filters=Filters.chat_type.groups, run_async=True)
 
 dispatcher.add_handler(BAN_HANDLER)
