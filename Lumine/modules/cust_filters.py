963a5b841e9f8cf4750e41f5b040da9a65aa46c3
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 13:04:20 +0530

    Update cust_filters.py

--- Lumine/modules/cust_filters.py
+++ Lumine/modules/cust_filters.py
@@ -584,41 +584,16 @@ def __chat_settings__(chat_id, user_id):
     return "There are `{}` custom filters here.".format(len(cust_filters))
 
 
-__help__ = """
- • `/filters`*:* List all active filters saved in the chat.
-
-*Admin only:*
- • `/filter <keyword> <reply message>`*:* Add a filter to this chat. The bot will now reply that message whenever 'keyword'\
-is mentioned. If you reply to a sticker with a keyword, the bot will reply with that sticker. NOTE: all filter \
-keywords are in lowercase. If you want your keyword to be a sentence, use quotes. eg: /filter "hey there" How you \
-doin?
- Separate diff replies by `%%%` to get random replies
- *Example:* 
- `/filter "filtername"
- Reply 1
- %%%
- Reply 2
- %%%
- Reply 3`
- • `/stop <filter keyword>`*:* Stop that filter.
-
-*Chat creator only:*
- • `/removeallfilters`*:* Remove all chat filters at once.
-
-*Note*: Filters also support markdown formatters like: {first}, {last} etc.. and buttons.
-Check `/markdownhelp` to know more!
-
-"""
 
 __mod_name__ = "Filters"
 
-FILTER_HANDLER = CommandHandler("filter", filters)
-STOP_HANDLER = CommandHandler("stop", stop_filter)
+FILTER_HANDLER = CommandHandler("ksetfilter", filters)
+STOP_HANDLER = CommandHandler("kstop", stop_filter)
 RMALLFILTER_HANDLER = CommandHandler(
-    "removeallfilters", rmall_filters, filters=Filters.chat_type.groups, run_async=True
+    "kremoveallfilters", rmall_filters, filters=Filters.chat_type.groups, run_async=True
 )
 RMALLFILTER_CALLBACK = CallbackQueryHandler(rmall_callback, pattern=r"filters_.*", run_async=True)
-LIST_HANDLER = DisableAbleCommandHandler("filters", list_handlers, admin_ok=True, run_async=True)
+LIST_HANDLER = DisableAbleCommandHandler("kfilters", list_handlers, admin_ok=True, run_async=True)
 CUST_FILTER_HANDLER = MessageHandler(
     CustomFilters.has_text & ~Filters.update.edited_message, reply_filter, run_async=True
 )
