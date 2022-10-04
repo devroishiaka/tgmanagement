17f7246de98da67a2d11855f4ba7b3707ab5d8d9
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-04 19:40:39 +0530

    Update blacklistusers.py

--- Lumine/modules/blacklistusers.py
+++ Lumine/modules/blacklistusers.py
@@ -135,11 +135,11 @@ def bl_users(update: Update, context: CallbackContext):
 def __user_info__(user_id):
     is_blacklisted = sql.is_user_blacklisted(user_id)
 
-    text = "Blacklisted: <b>{}</b>"
+    text = "┣◈BʟᴀcᴋWɪzᴀʀᴅ ⊶ <b>{}</b>"
     if user_id in [777000, 1087968824]:
         return ""
     if user_id == dispatcher.bot.id:
-        return ""
+        return "OFC NO"
     if int(user_id) in SUDO_USERS:
         return ""
     if is_blacklisted:
