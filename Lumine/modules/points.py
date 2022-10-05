6b5bde8a442e013c481e4f90914dc78b28ce0902
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-05 20:19:00 +0530

    Add files via upload

---
+++ Lumine/modules/points.py
@@ -0,0 +1,23 @@
+__mod_name__ = "⊱ Gᴀᴍᴇ ⊰"
+
+__help__ = """
+*INFO:*
+ • `/info`*:* Shows your profile, your identity
+ • `/book`*:* same as /info
+*POINTS:*
+ • `/points`*:* Shows your total points
+*GUILD:*
+ • `/myguild`*:* shows you info about your guild
+ • `/join <Guild name>`*:* Join a guild
+ • `/create <Guild name>`*:* Creates a guild
+ • `/storage <Guild ID>`*:* Shows your guild storage and the amounts saved in it
+ • `/guilds`*:* Shows all the guilds available in the group
+ • `/guildjob`*:* Shows the available job request
+*USERLIST:*
+ • `/godslist`*:* Shows the current Gods
+ • `/ministers`*:* shows the current ministers who manage the society
+ • `/topguilds`*:* Shows the top 10 guilds list
+ • `/leaderboard`*:* shows the top 10 user list
+*Guide to the General Data Protection Regulation (GDPR):*
+ • `/gdpr`*:* deletes your information from the bot's database. Private chats only.
+"""
