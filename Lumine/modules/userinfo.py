d0257fb1bef461364a69bf278d213031868f95e6
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-05 20:40:44 +0530

    Update userinfo.py

--- Lumine/modules/userinfo.py
+++ Lumine/modules/userinfo.py
@@ -231,7 +231,7 @@ def info(update: Update, context: CallbackContext):
         except TypeError:
             mod_info = mod.__user_info__(user.id, chat.id).strip()
         if mod_info:
-            text += "\n\n┃" + mod_info
+            text += "\n┃\n" + mod_info
 
     if INFOPIC:
         try:
