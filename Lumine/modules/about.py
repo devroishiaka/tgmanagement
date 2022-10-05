0679e4edaf2aefca8a29876c463a4b1bed947404
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-05 21:13:18 +0530

    Update about.py

--- Lumine/modules/about.py
+++ Lumine/modules/about.py
@@ -4,12 +4,12 @@ __help__ = """
 Hello everyone,
 I’m Eri Akane. Nice to meet you.
 I’m a fun bot specially made for [Friends Domain Group](t.me/FriendsDomain).
+My developer and owner is [Ishikki Akabane](t.me/ishikki_akabane)✨.
 
-My developer and owner is [Ishikki Akabane](t.me/ishikki_akabane).
-Currently I’m still in beta phase which means you can’t add me in your groups. If you still wish to add me in your groups you must talk with @ishikki_akabane.
+Currently I’m still in beta phase which means you can’t add me in your groups. If you still wish to add me in your groups you must talk with Ishikki Akabane.
 You can see me fully working in @friendsDomain group, please visit the group and play with me.
-if you want to know about the game or how to play, then please send /howto
+if you want to know about the game or how to play, then please see the guide
 
-If you have any doubts then simply message my babe [Ishikki Akabane](t.me/ishikki_akabane)
+If you have any doubts then simply message my bestie [Ishikki Akabane](t.me/ishikki_akabane)
 """
 __mod_name__ = "⊱ About ⊰"
