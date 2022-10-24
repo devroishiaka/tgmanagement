af9e6e5c5ea5fb7ce48bd6b312b8d83c612a2bf5
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-24 14:12:15 +0530

    Update __main__.py

--- Lumine/__main__.py
+++ Lumine/__main__.py
@@ -23,6 +23,7 @@ from Lumine import (
     updater,
 )
 
+
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from Lumine.modules import ALL_MODULES
@@ -739,6 +740,16 @@ def migrate_chats(update: Update, context: CallbackContext):
 
 
 def main():
+    if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
+        try:
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "Eʀɪ Aᴋᴀɴᴇ ᴜᴘᴅᴀᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ \nRᴜɴɴɪɴɢ sᴍᴏᴏᴛʜʟʏ ʟɪᴋᴇ ʙᴇғᴏʀᴇ (◍•ᴗ•◍)", parse_mode=ParseMode.MARKDOWN,)
+        except Unauthorized:
+            LOGGER.warning(
+                "Bot isnt able to send message to support_chat, go and check!"
+            )
+        except BadRequest as e:
+            LOGGER.warning(e.message)
+
     test_handler = CommandHandler("test", test, run_async=True)
     start_handler = CommandHandler("start", start, run_async=True)
 
