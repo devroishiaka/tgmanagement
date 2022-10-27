d2857c447bef8beea66e4734d7f6c1d3ca3c91eb
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-27 16:06:46 +0530

    Update mongo_inline.py

--- Lumine/modules/mongo_inline.py
+++ Lumine/modules/mongo_inline.py
@@ -41,8 +41,70 @@ def help1btn_callback(update: Update, context: CallbackContext):
         )
 
 
+def friendx(update: Update, context: CallbackContext):
+    message = update.effective_message
+    sender_id = update.effective_user.id
+    sender_name = update.effective_user.first_name
+    if message.reply_to_message:
+        repl_message = message.reply_to_message
+        user_id = repl_message.from_user.id
+        user_name = repl_message.from_user.first_name
+        message.reply_text(
+            f"[{user_name}](tg://openmessage?user_id={user_id}) Do you agree ?",
+            reply_to_message_id=user_id,
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(text="✅️", callback_data=f"yess={user_id}")
+                        InlineKeyboardButton(text="❌", callback_data=f"nooo={user_id}")
+                    ]
+                ]
+            ),
+            parse_mode=ParseMode.HTML,
+        )
+    else:
+        message.reply_text(f"You not registerd!!!\nPlease use /register command to register yourself")
+
+
+def friendbtn_callback(update: Update, context: CallbackContext):
+    message = update.effective_message
+    query = update.callback_query
+    bot = context.bot
+    user = update.effective_user.id
+    splitter = query.data.split("=")
+    query_match = splitter[0]
+    if query_match == "yess":
+        user_id = splitter[1]
+        if user_id == user:
+            query.message.edit_text(
+                "Congratulations🎊"
+            )
+        else:
+            bot.answer_callback_query(
+                query.id,
+                text="Its not your request"
+            )
+    else:
+        user_id = splitter[1]
+        if user_id == user:
+            query.message.edit_text(
+                "Sad lyf"
+            )
+        else:
+            bot.answer_callback_query(
+                query.id,
+                text="Its not your request"
+            )
+
+
+
+
 HELP_1_HANDLER = CommandHandler("help1", help1, run_async=True)
 HELP_1_BTN_HANDLER = CallbackQueryHandler(help1btn_callback)
+FRIENDSX_HANDLER = CommandHandler("addfriend", friendx, run_async=True)
+FRIENDSX_BTN_HANDLER = CallbackQueryHandler(friendbtn_callback)
 
 dispatcher.add_handler(HELP_1_HANDLER)
 dispatcher.add_handler(HELP_1_BTN_HANDLER)
+dispatcher.add_handler(FRIENDSX_HANDLER)
+dispatcher.add_handler(FRIENDSX_BTN_HANDLER)
