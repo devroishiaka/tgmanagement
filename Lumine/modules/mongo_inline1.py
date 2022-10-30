b26fb0da7b021d79e6510676f8487b2d9260ccaf
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-10-30 16:33:31 +0530

    Update mongo_inline1.py

--- Lumine/modules/mongo_inline1.py
+++ Lumine/modules/mongo_inline1.py
@@ -22,7 +22,7 @@ def bann(update: Update, context: CallbackContext) -> str:
         useridd = int(user_id)
         bot.sendMessage(
             chat.id,
-            text = "Choose"
+            text = "Choose",
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
