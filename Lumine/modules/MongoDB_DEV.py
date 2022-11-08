403c3e91d9e0c03035d92144346c362c6d1a55a1
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-11-08 17:32:46 +0530

    Create MongoDB_DEV.py

---
+++ Lumine/modules/MongoDB_DEV.py
@@ -0,0 +1,55 @@
+from Lumine.modules.MongoDB import collection1, collection2
+
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
+from telegram import MAX_MESSAGE_LENGTH, ParseMode, Update, MessageEntity
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.utils.helpers import escape_markdown, mention_html
+
+from Lumine import (
+    dispatcher
+)
+
+from Lumine.modules.helper_funcs.chat_status import sudo_plus, gods_plus
+from Lumine.modules.helper_funcs.extraction import extract_user
+from Lumine.modules.disable import DisableAbleCommandHandler
+
+
+def devregister(update: Update, context: CallbackContext):
+    message = update.effective_message
+    splitter = message.text.split(None, 1)[1]
+    list_of_words = splitter.split("-")
+    user_id = int(list_of_words[0])
+    name = list_of_words[1]
+    post_dict1 = {"_id": user_id, "Name": name, "Level": 1, "Rank": "D-Class", "Points": 100, "Gender": "No", "Partner": "No", "Friend": "No", "Father": "No", "Mother": "No", "Children": "No", "Status": "No", "Bounty": 0}
+    collection1.insert_one(post_dict1)
+    message.reply_text(f"#Terminal\n<code>Operator Command =</code> <b>Register</b>\n<code>Successfully Registered the user</code> <b>{name}</b>", parse_mode=ParseMode.HTML)
+
+
+
+
+
+
+
+
+
+
+
+DEVREGISTER_HANDLER = DisableAbleCommandHandler("dregister", devregister, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+#_HANDLER = DisableAbleCommandHandler(, run_async=True)
+
+
+dispatcher.add_handler(DEVREGISTER_HANDLER)
+#dispatcher.add_handler()
+#dispatcher.add_handler()
+#dispatcher.add_handler()
+#dispatcher.add_handler()
+#dispatcher.add_handler()
+#dispatcher.add_handler()
+#dispatcher.add_handler()
+#dispatcher.add_handler()
