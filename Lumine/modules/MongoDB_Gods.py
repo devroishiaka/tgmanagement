3e9b0e6a37889be4fb6706cfa5ec6bc6a4225122
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-11-12 13:25:26 +0530

    Update MongoDB_Gods.py

--- Lumine/modules/MongoDB_Gods.py
+++ Lumine/modules/MongoDB_Gods.py
@@ -20,13 +20,18 @@ def createguildx(update: Update, context: CallbackContext):
     message = update.effective_message
     sender_id = update.effective_user.id
     text = message.text
-    splitter = text.split(None, 1)[1]
-    guild_id = splitter.split(" ")[0]
-    guild_name = splitter.split(" ")[1]
+    if len(text) == 2:
+        message.reply_text("Correct format:\n/create <user ID> <Guild Name>")
+    elif len(text) > 2:
+        splitter = text.split(None, 1)[1]
+        guild_id = splitter.split(" ")[0]
+        guild_name = splitter.split(None, 1)[1]
 
-    collection2.insert_one({"_id": guild_id, "Guild_Name": guild_name, "Guild_Level": 1, "Members": 1, "Vault": 1000, "Guild_Creator": "Gods", "Crime_Rate": 0, "Guild_Rank": 1, "Guild_Status": "No", "Guild_Pfp": "No"})
-    collection1.update_one({"_id": guild_id}, {"$inc": {"Points": -1000}})
-    message.reply_text("Created a new guild!")
+        collection2.insert_one({"_id": guild_id, "Guild_Name": guild_name, "Guild_FName": guild_name, "Guild_Level": 1, "Members": 1, "Vault": 2000, "Guild_Creator": "Gods", "Crime_Rate": 0, "Guild_Rank": 1, "Guild_Status": "No", "Guild_Pfp": "No" })
+        collection1.update_one({"_id": guild_id}, {"$inc": {"Points": -1000}})
+        message.reply_text("✅ Created a new guild!")
+    else:
+        message.reply_text("Correct format:\n/create <user ID> <Guild Name>")
 
 
 
@@ -44,25 +49,25 @@ def setpointsx(update: Update, context: CallbackContext):
         point1 = list_of_words[1]
         points1 = int(point1)
         collection1.update_one({"_id": user_id}, {"$inc": {"Points": points1}})
-        message.reply_text("successfully updated the points")
+        message.reply_text("✅ successfully updated the points")
         
     if len(list_of_words) == 3:
         id_tag = list_of_words[1]
         point2 = list_of_words[2]
         points2 = int(point2)
         collection1.update_one({"_id": id_tag}, {"$inc": {"Points": points2}})
-        message.reply_text("successfully updated the points")
+        message.reply_text("✅ successfully updated the points")
         
     elif len(list_of_words) == 2:
         point3 = list_of_words[1]
         points3 = int(point3)
         collection1.update_one({"_id": sender_id}, {"$inc": {"Points": points3}})
-        message.reply_text("successfully updated the points")
+        message.reply_text("✅ successfully updated the points")
 
 
 #/setowner
 @gods_plus
-def setownerx(update: Update, context: CallbackContext):
+def setowner(update: Update, context: CallbackContext):
     message = update.effective_message
     sender_id = update.effective_user.id
     text = message.text
@@ -73,7 +78,7 @@ def setownerx(update: Update, context: CallbackContext):
     results = collection1.find_one({"_id": user_id})
     result = results["Name"]
     collection2.update_one({"_id": user_id}, {"$set": {"Guild_Creator": result}})
-    message.reply_text("successfully updated The Guild INFO")
+    message.reply_text("✅ successfully updated The Guild INFO")
 
     
 @gods_plus
@@ -81,42 +86,52 @@ def checkdata(update: Update, context: CallbackContext):
     message = update.effective_message
     sender_id = update.effective_user.id
     text = message.text
-    splitter = text.split(
-        None, 1
-    )
-    user_id = int(splitter[1])
-    results = collection1.find_one({"_id": user_id})
-    name = results["Name"]
-    level = results["Level"]
-    rank = results["Rank"]
-    points = results["Points"]
-    gender = results["Gender"]
-    partner = results["Partner"]
-    friend = results["Friend"]
-    father = results["Father"]
-    mother = results["Mother"]
-    children = results["Children"]
-    bounty = results["Bounty"]
-    status = results["Status"]
+    if len(text) > 1:
+        splitter = text.split(
+            None, 1
+        )
+        user_id = splitter[1]
+        results = collection1.find_one({"_id": user_id})
+        if results:
+            name = results["Name"]
+            level = results["Level"]
+            rank = results["Rank"]
+            points = results["Points"]
+            gender = results["Gender"]
+            partner = results["Partner"]
+            friend = results["Friend"]
+            father = results["Father"]
+            mother = results["Mother"]
+            children = results["Children"]
+            bounty = results["Bounty"]
+            status = results["Status"]
 
-    message.reply_text(f"""
-USERDATA
+            message.reply_text(f"""
+━━━━━━━━━҉━━━━━━━━━
+<b>USERDATA</b>
 
-USER ID = {user_id}
-USER NAME = {name}
-LEVEL = {level}
-RANK = {rank}
-POINTS = {points}
-GENDER = {gender}
-FRIEND = {friend}
-PARTNER = {partner}
-FATHER = {father}
-MOTHER = {mother}
-CHILDREN = {children}
-BOUNTY = {bounty}
-GUILD = {status}
-"""
-    )
+USER ID = <code>{user_id}</code>
+USER NAME = <code>{name}</code>
+LEVEL = <code>{level}</code>
+RANK = <code>{rank}</code>
+POINTS = <code>{points}</code>
+GENDER = <code>{gender}</code>
+FRIEND = <code>{friend}</code>
+PARTNER = <code>{partner}</code>
+FATHER = <code>{father}</code>
+MOTHER = <code>{mother}</code>
+CHILDREN = <code>{children}</code>
+BOUNTY = <code>{bounty}</code>
+GUILD = <code>{status}</code>
+━━━━━━━━━҉━━━━━━━━━
+""",
+                parse_mode=parse_mode.HTML
+            )
+        else:
+            message.reply_text("NO such user in my Database")
+    else:
+        message.reply_text("Wrong format\nUse this way /checkdata <user ID>")
+       
 
 
 @gods_plus
@@ -132,9 +147,9 @@ def addpfp(update: Update, context: CallbackContext):
     results = collection2.find_one({"Guild_Name": guild_name})
     if results:
         collection2.update_one({"Guild_Name": guild_name}, {"$set": {"Guild_Pfp": pfplink}})
-        message.reply_text("Successfully updated the Guild Profile Pic")
+        message.reply_text("✅ Successfully updated the Guild Profile Pic")
     else:
-        message.reply_text("NO such GUILD found!!")
+        message.reply_text("NO such GUILD found!!
         
         
 
