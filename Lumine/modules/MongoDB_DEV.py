f849a70bff6e3fbec12d4895e2ceb2de7423dea9
devroishiaka <108065240+devroishiaka@users.noreply.github.com>
2022-11-15 16:11:29 +0530

    Update MongoDB_DEV.py

--- Lumine/modules/MongoDB_DEV.py
+++ Lumine/modules/MongoDB_DEV.py
@@ -16,10 +16,9 @@ from Lumine.modules.disable import DisableAbleCommandHandler
 
 def devregister(update: Update, context: CallbackContext):
     message = update.effective_message
-    splitter = message.text.split(None, 1)[1]
-    list_of_words = splitter.split("-")
-    user_id = int(list_of_words[0])
-    name = list_of_words[1]
+    list_of_words = message.text.split(" ")
+    user_id = int(list_of_words[1])
+    name = list_of_words[2]
     post_dict1 = {"_id": user_id, "Name": name, "EXP": 10, "Level": 1, "Rank": "D-Class", "Points": 100, "Gender": "No", "Partner": "No", "Friend": "No", "Father": "No", "Mother": "No", "Children": "No", "Status": "No", "Bounty": 0, "Deposit": 0, "TDeposit": 0}
     collection1.insert_one(post_dict1)
     message.reply_text(f"#Terminal\n<code>Operator Command =</code> <b>Register</b>\n<code>Successfully Registered the user</code> <b>{name}</b>", parse_mode=ParseMode.HTML)
